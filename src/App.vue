25edbbb13d87b0bfbb00244e23d123e677dfdf60
Jared Peter <jaredpeter06@icloud.com>
2022-08-12 05:27:44 -0300

    Added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="h-screen relative bg-[url('./hero.png')] bg-cover bg-no-repeat flex justify-center items-center">
+    <div class="w-screen h-screen absolute top-0 left-0 backdrop-blur-sm z-10"></div>
+    <div class="relative z-20 bg-white rounded-2xl py-8 px-10 shadow-2xl h-96">
+      <div class="flex flex-col justify-center items-center h-full">
+        <h1 class="text-5xl font-bold">Motores de Recomendación</h1>
+        <h4 class="text-xl my-3 font-light">Por Máximo Giqueaux, Jared Peter, Giovanni Gerber</h4>
+        <button
+          class="px-6 py-2 rounded-lg shadow-sm bg-red-400 focus:ring-2 focus:ring-red-300 focus:ring-offset-2 text-white">
+          Siguiente
+        </button>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
